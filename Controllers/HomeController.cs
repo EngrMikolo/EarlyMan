7bd54000ed098487499aab167998e5904fd236a2
David Okeke <davidokeke.c@gmail.com>
2020-07-01 22:11:05 +0100

    Add base MVC structure

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telefin.Models;
+
+namespace Telefin.Controllers
+{
+    public class HomeController: Controller
+    {
+       public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Prints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Telefin.Models
+{
+    public class Prints
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Color { get; set; }
+        public string MaterialType { get; set; }
+        public int Size { get; set; }
+        public string Categories { get; set; }
+    }
+}
