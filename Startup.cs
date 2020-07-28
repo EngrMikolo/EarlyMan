1570dac52d1231fb9c227d89e775c5ffd4203c1f
David Okeke <davidokeke.c@gmail.com>
2020-07-28 23:22:28 +0100

    Revert "Add path to container field"
    This reverts commit a82b43140b1ece4135ba056fdcdabc3d09286ee2.

--- Startup.cs
+++ Startup.cs
@@ -1,39 +1,41 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Telefin.Models;
 
 namespace Telefin
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) =>
-        Configuration = configuration;
+        public Startup(IConfiguration config) => Configuration = config;
 
         public IConfiguration Configuration { get; }
-
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(
-            Configuration["Data:3dProdLocation:ConnectionString"]));
             services.AddTransient<IPrintRepository, EFPrintRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                 options.UseSqlServer(
+                     Configuration["Data:3dProdLocation:ConnectionString"]));
+
             services.AddMvc();
         }
-            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-            public void Configure(IApplicationBuilder app)
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-                app.UseStatusCodePages();
-                app.UseStaticFiles();
-                app.UseMvc(routes =>
-                {
-                    routes.MapRoute(
-                        name: "default",
-                        template: "{controller=Print}/{action=Index}");
-                });
-                SeedData.EnsurePopulated(app);
-            }
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Print}/{action=Index}");
+                    
+            });
         }
     }
+}
