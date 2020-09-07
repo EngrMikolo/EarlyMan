5e8d2a4bea2d02e0bfa7d885fee1ac21348f1d30
David Okeke <davidokeke.c@gmail.com>
2020-09-07 21:25:24 +0100

    Add User model

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Telefin.Models;
 
 namespace Telefin.Controllers
 {
     public class AdminController : Controller
     {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public ViewResult Index() => View(userManager.Users);
+
         public ViewResult Login() => View();
     }
 }
